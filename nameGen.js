f72ad1f1af976b5d87c18fe258a6970642034140
DonChatelain <djchatelain@gmail.com>
2016-05-16 15:27:14 -0700

    nameGen() now produces random animals along with random number from 1 - 1000

--- nameGen.js
+++ nameGen.js
@@ -1,6 +1,27 @@
+const animals = [  //18 total
+  'gerenuk',
+  'blobfish',
+  'dugong',
+  'babirusa',
+  'lamprey',
+  'fossa',
+  'yeticrab',
+  'spidercrab',
+  'batfish',
+  'goblinshark',
+  'panda-ant',
+  'tenrec',
+  'hawkmoth',
+  'mantis-shrimp',
+  'isopod',
+  'parrotfish',
+  'narwhal',
+  'seapig'
+];
+
 module.exports = function() {
+  const prefix = animals[Math.floor(Math.random() * animals.length)];
   const suffix = Math.ceil(Math.random().toFixed(2) * 1000); // Random 1 - 1000
-  return `Guest-${suffix}`;
+  return `${prefix}-${suffix}`;
 };
 
-// Nothing yet to prevent two sockets from generating same numbers
