26687b877d4b957c8523443a9dbce8d76bd230db
Allison Davis <a.rae.davis@gmail.com>
2016-05-16 15:55:00 -0700

    refactor; create new handle function

--- index.js
+++ index.js
@@ -9,34 +9,28 @@ var onlineUsers = []; //used to display other users online
 
 const server = net.createServer((socket) => {
   //assigns random number to socket.name
-  socket.name = 'jedi-' + Math.floor(Math.random() * 10000);
+  socket.name = newHandle();
 
   var userName = socket.name;
 
-  //adds socket.name to array of online clients
+  broadcast(socket.name, 'has joined.');
   clients.push(socket);
 
-  broadcast(socket.name, 'has joined.');
-
-  //write to client upon connecting
+  //welcome message to new client
   socket.write(`May the force be with you, ${socket.name}!\n` );
-
   if(onlineUsers.length > 0){
     socket.write(onlineUsers.join(', ') + ' are also online.\n');
   } else {
     socket.write('you are the only user online\n');
   }
 
-  //adds current user to those online
+  //adds new client to those online
   onlineUsers.push(socket.name);
 
 
+  //event emitter for new socket data
   socket.on('data', (chunk) =>{
-
     broadcast(`${userName}: `, chunk);
-
-    //output to log file
-    log.write(`${socket.name}: ${chunk.toString()}\n`);
   });
 
   //on disconnect, notes disconnect and user name on server, removes departing client from client array
@@ -49,6 +43,10 @@ const server = net.createServer((socket) => {
   });
 });
 
+var newHandle = function(){
+  return 'jedi-' + Math.floor(Math.random() * 10000);
+};
+
 var broadcast = function(currentUser, message){
   clients.forEach((user) =>{
     if(currentUser !== `${user.name}: `){
@@ -56,12 +54,12 @@ var broadcast = function(currentUser, message){
     }
   });
   console.log(`${currentUser} ${message.toString()}\n`);
+  log.write(`${currentUser} ${message.toString()}\n`);
 };
 
-//TODO refactor to manage clients separately from server creation logic
-
-//DONE create broadcast functionality
-//DONE remove a user from clients array on disconnect
+// var requestNewHandle = function(currentUser){
+//
+// };
 
 server.listen(8000, () =>{
   console.log('server listening at %j', server.address());
