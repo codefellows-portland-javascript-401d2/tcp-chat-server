d5d18095e94062e714bc05e292559271b9340fcb
Yvonne Hayes <queyvonne@gmail.com>
2016-05-16 14:10:37 -0700

    created server, made it listen, made clients array, push new clients into that, wrote function to broadcast messages to all, welcomenewbies and say goodbye

--- server.js
+++ server.js
@@ -1,8 +1,37 @@
 const net = require( 'net' );
 
+//an array to push the clients into
+var clients = [];
 
 const server = net.createServer( socket => {
 
+  //identifying each client
+  socket.name = socket.remoteAddress + ':' + socket.remotePort;
+
+  //pushing client into the array
+  clients.push(socket);
+
+  //function tellAll that broadcasts messages everybody
+  function tellAll(message, sender) {
+    clients.forEach(function(client) {
+      client.write(message);
+    });
+  }
+
+  //Welcome new client and announce arrival to everybody
+  socket.write('Welcome' + socket.name + '\n');
+  tellAll(socket.name + ' has joined the chat. Say hello!\n', socket);
+
+  //broadcast client message to everybody
+  socket.on('data', function (data) {
+    tellAll(socket.name + ': ' + data, socket);
+  });
+
+  //remove client from the array when they leave the chat
+  socket.on('end', function (data) {
+    clients.splice(clients.indexOf(socket), 1);
+    tellAll(socket.name + ' has left the chat. Awwwwww :( \n');
+  });
 });
 
 server.listen( 65000, () => {
