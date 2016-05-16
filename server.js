5045a8e35ac51af79a01b9ddeafa63b7b552cd27
Yvonne Hayes <queyvonne@gmail.com>
2016-05-16 14:55:06 -0700

    added random name

--- server.js
+++ server.js
@@ -6,12 +6,16 @@ var clients = [];
 const server = net.createServer( socket => {
 
   //identifying each client
-  socket.name = socket.remoteAddress + ':' + socket.remotePort;
+  socket.name = randomName();
+
+  // socket.name = socket.remoteAddress + ':' + socket.remotePort;
+  // var userName = socket.name;
+  // var userName = randomName();
 
   //pushing client into the array
   clients.push(socket);
 
-  //function tellAll that broadcasts messages everybody
+  //function tellAll that broadcasts messages to everybody
   function tellAll(message, sender) {
     clients.forEach(function(client) {
       client.write(message);
@@ -19,7 +23,7 @@ const server = net.createServer( socket => {
   }
 
   //Welcome new client and announce arrival to everybody
-  socket.write('Welcome' + socket.name + '\n');
+  socket.write('Welcome to the chat, ' + socket.name + '\n');
   tellAll(socket.name + ' has joined the chat. Say hello!\n', socket);
 
   //broadcast client message to everybody
@@ -32,8 +36,18 @@ const server = net.createServer( socket => {
     clients.splice(clients.indexOf(socket), 1);
     tellAll(socket.name + ' has left the chat. Awwwwww :( \n');
   });
+
+  //creating random user name
+
+  var randomName = function() {
+
+    var animals = ['pigeon', 'seagull', 'bat', 'owl', 'sparrow', 'hawk', 'fish', 'frog', 'whale', 'shark', 'octopus', 'rabbit', 'chipmunk', 'dog', 'cat', 'lynx', 'mouse', 'lion', 'moose', 'horse', 'deer', 'raccoon', 'zebra', 'goat', 'cow', 'pig', 'tiger', 'wolf', 'pony', 'antelope', 'buffalo', 'camel', 'donkey', 'elk', 'fox', 'monkey', 'gazelle', 'jaguar', 'leopard', 'lemur', 'yak', 'elephant', 'giraffe', 'hippopotamus', 'rhinoceros', 'grizzlybear'];
+    var colors = ['silver', 'gray', 'black', 'red', 'maroon', 'olive', 'lime', 'green', 'teal', 'blue', 'navy', 'fuchsia', 'purple'];
+    return colors[Math.floor(Math.random() * colors.length)] + '_' + animals[Math.floor(Math.random() * animals.length)];
+  };
+
 });
 
-server.listen( 65000, () => {
-  console.log('Server has been opened on Port 65000');
+server.listen( 6500, () => {
+  console.log('Server has been opened on Port 6500');
 });
