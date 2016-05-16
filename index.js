edeb2e12853380de88c229dcf8ee275703cc33df
Allison Davis <a.rae.davis@gmail.com>
2016-05-16 13:42:48 -0700

    begin broadcast method

--- index.js
+++ index.js
@@ -3,24 +3,37 @@ var fs = require('fs');
 
 const log = fs.createWriteStream('chat-log.txt');
 
+// const chat = fs.createWriteStream();
+
 var clients = [];
 
 
 const server = net.createServer((socket) => {
+  //assigns random number to socket.name
   socket.name = 'jedi-' + Math.floor(Math.random() * 10000);
 
-  //TODO socket names are not being pushed properly to client array - why??
-  clients.push[socket.name];
+  //adds socket.name to array of online clients
+  clients.push(socket.name);
 
+  //outputs to server
   console.log(`${socket.name} connected`);
 
+  //outputs to client when they join
   socket.write(`May the force be with you, ${socket.name}!\n` );
-  socket.write(clients.join('') + ' are currently online.\n');
+  socket.write(clients.join(', ') + ' are currently online.\n');
 
+
+  //on client message
   socket.on('data', (chunk) =>{
+
+    //broadcast to all chat users
+    // broadcast(chunk);
+
+    //output to log file
     log.write(`${socket.name}: ${chunk.toString()}\n`);
   });
 
+  //on disconnect, notes disconnect and user name on server, removes departing client from client array
   socket.on('close', () =>{
     console.log(`${socket.name} disconnected`);
     var index = clients.indexOf(socket.name);
@@ -28,14 +41,21 @@ const server = net.createServer((socket) => {
     clients.splice(index, 1);
   });
 
+  function broadcast(message){
+    clients.forEach(() => {
+      chat.write(message);
+    });
+  }
+
+  //pipes socket output to console
   socket.pipe(process.stdout);
 });
 
+
 //TODO managing clients should be separate from server creation logic
-//TODO how to clear chat-log - currently clears when new users connect
 
 //TODO create broadcast function
-//TODO remove a user from clients array on disconnect
+//DONE remove a user from clients array on disconnect
 
 server.listen(8000, () =>{
   console.log('server listening at %j', server.address());
