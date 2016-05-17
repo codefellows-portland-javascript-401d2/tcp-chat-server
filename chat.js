a472755cbe34ac8c87fffdfc2f98f655371dd41c
Johnny Luangphasy <johnny@jluangphasy.com>
2016-05-17 01:29:49 -0700

    Returning messages in all sockets

--- chat.js
+++ chat.js
@@ -1,27 +1,47 @@
 const net = require('net');
 const superheroes = require('superheroes');
 
-function createServer(portNumber = 65000) {
-  const server = net.createServer(socket => {
-    const name = superheroes.random();
+let chat = {};
+let allSockets = [];
+let allNames = [];
 
-    console.log(`${name} joined our team.`);
+function broadcastSockets(socket, content) {
+  allSockets.forEach((currentSocket) => {
+    if (currentSocket.superhero !== socket.superhero) {
+      currentSocket.write(content);
+    }
+  });
+}
+
+function createServer(portNumber = 65000) {
+  const server = net.createServer((socket) => {
+    socket.superhero = superheroes.random();
+
+    allSockets.push(socket);
+
+    broadcastSockets(socket, `\n${socket.superhero} joined our team.\n\nWelcome to Superheroes, ${socket.superhero}!\n\n${socket.superhero}:\n`);
 
     socket.on('data', chunkData => {
-      console.log(`${name}: ${chunkData.toString()}`);
+      let chunkStr = chunkData.toString().trim();
+
+      broadcastSockets(socket, `${socket.superhero}: ${chunkStr}\n`);
+
+      socket.write(`${socket.superhero}: `);
     });
 
     socket.on('close', () => {
-      console.log(`${name} left our team.`);
+      broadcastSockets(socket, `${socket.superhero} left our team.`);
     });
+  });
 
-    // socket.pipe(process.stdout);
+  server.on('error', (err) => {
+    throw err;
   });
 
   server.listen(portNumber, () => {
     let address = server.address();
 
-    console.log(`Superheroes at port ${address.port} on localhost (http://localhost:${address.port}).`);
+    process.stdout.write(`Superheroes at port ${address.port} on localhost (http://localhost:${address.port}).`);
   });
 }
 
