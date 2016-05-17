ce675b8d3bbb20e8a542ce1f479ffec5bef415eb
Johnny Luangphasy <johnny@jluangphasy.com>
2016-05-17 01:39:15 -0700

    Refactored chat module

--- chat.js
+++ chat.js
@@ -2,49 +2,42 @@ const net = require('net');
 const superheroes = require('superheroes');
 
 let chat = {};
-let allSockets = [];
-let allNames = [];
 
-function broadcastSockets(socket, content) {
-  allSockets.forEach((currentSocket) => {
+chat.allSockets = [];
+
+chat.broadcastSockets = function (socket, content) {
+  chat.allSockets.forEach((currentSocket) => {
     if (currentSocket.superhero !== socket.superhero) {
       currentSocket.write(content);
     }
   });
-}
+};
 
-function createServer(portNumber = 65000) {
-  const server = net.createServer((socket) => {
+chat.createServer = function (portNumber = 65000) {
+  net.createServer((socket) => {
     socket.superhero = superheroes.random();
 
-    allSockets.push(socket);
+    chat.allSockets.push(socket);
 
-    broadcastSockets(socket, `\n${socket.superhero} joined our team.\n\nWelcome to Superheroes, ${socket.superhero}!\n\n${socket.superhero}:\n`);
+    chat.broadcastSockets(socket, `\n${socket.superhero} joined our team.\n\nWelcome to Superheroes, ${socket.superhero}!\n`);
 
     socket.on('data', chunkData => {
       let chunkStr = chunkData.toString().trim();
 
-      broadcastSockets(socket, `${socket.superhero}: ${chunkStr}\n`);
+      chat.broadcastSockets(socket, `${socket.superhero}: ${chunkStr}\n`);
 
       socket.write(`${socket.superhero}: `);
     });
 
     socket.on('close', () => {
-      broadcastSockets(socket, `${socket.superhero} left our team.`);
+      chat.broadcastSockets(socket, `${socket.superhero} left our team.`);
     });
-  });
-
-  server.on('error', (err) => {
+  }).on('error', (err) => {
     throw err;
+  }).listen(portNumber, () => {
+    process.stdout.write(`Superheroes at port ${portNumber} on localhost `
+      + `(http://localhost:${portNumber}).`);
   });
-
-  server.listen(portNumber, () => {
-    let address = server.address();
-
-    process.stdout.write(`Superheroes at port ${address.port} on localhost (http://localhost:${address.port}).`);
-  });
-}
-
-module.exports = {
-  createServer
 };
+
+module.exports = chat;
